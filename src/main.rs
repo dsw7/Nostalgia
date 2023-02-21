0a1d5398bf6e08876792ec145ca03612995c2989
David Weber <dsw7@sfu.ca>
2023-02-21 00:30:46 -0800

    Add basic pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+const R1: f32 = 99.5 * 1000.;
+const R2: f32 = 21.5 * 1000.;
+
+fn compute_frequency(period: &f32) -> f32 {
+    1. / period
+}
+
+fn compute_capacitance(freq: &f32) -> f32 {
+    1.44 / (freq * (R1 + 2. * R2))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let p = 5.00 / 1000.;
+
+    let f = compute_frequency(&p);
+    let c = compute_capacitance(&f);
+
+    println!("{}", c);
 }
