265697b3fc7fc1c4c0f19875bb6626feaedc7333
David Weber <dsw7@sfu.ca>
2023-02-23 15:24:22 -0800

    Update tests

--- tests/test_binary.py
+++ tests/test_binary.py
@@ -1,10 +1,21 @@
+import typing
 import os
 import subprocess
 import pytest
 
 PATH_BINARY = os.path.join(os.getcwd(), 'target/debug/nos')
 
-@pytest.mark.parametrize('args', [PATH_BINARY, f'{PATH_BINARY} --period=abc'])
-def test_invalid_args(args: str) -> None:
+if not os.path.exists(PATH_BINARY):
+    pytest.exit(f'{PATH_BINARY} does not exist!')
+
+TEST_CASES_1 = [
+    [PATH_BINARY],
+    [PATH_BINARY, '--period=abc'],
+    [PATH_BINARY, '-p=abc'],
+    [PATH_BINARY, '--period=5', '--unit=foo']
+]
+
+@pytest.mark.parametrize('command', TEST_CASES_1, ids=[' '.join(s) for s in TEST_CASES_1])
+def test_invalid_args(command: typing.List[str]) -> None:
     with pytest.raises(subprocess.CalledProcessError):
-        subprocess.check_output(args.split(), stderr=subprocess.DEVNULL)
+        subprocess.check_output(command, stderr=subprocess.DEVNULL)
