3596d9ed84fdc45f0e14d188473d330960dbe251
David Weber <dsw7@sfu.ca>
2023-02-24 01:19:52 -0800

    Manually write json

--- src/routers.rs
+++ src/routers.rs
@@ -1,24 +1,23 @@
 use std::fs;
 use std::env;
 use std::path::Path;
-
 use crate::consts::Results;
 
 pub fn export_data_to_file(results: &Results) {
 
     let mut outgoing = String::new();
 
-    outgoing.push_str("# Nostalgia generated results file. This file can be safely deleted\n");
-    outgoing.push_str("# See https://github.com/dsw7/Nostalgia for more information\n");
-
-    outgoing.push_str(&format!("Frequency (Hz): {}\n", results.frequency));
-    outgoing.push_str(&format!("Period (s): {}\n", results.period));
-    outgoing.push_str(&format!("Capacitance (F): {}\n", results.cap_f));
-    outgoing.push_str(&format!("Capacitance (uF): {}\n", results.cap_uf));
-    outgoing.push_str(&format!("Capacitance (nF): {}\n", results.cap_nf));
+    outgoing.push_str("{");
+    outgoing.push_str("\"description\":\"See https://github.com/dsw7/Nostalgia for more information\",");
+    outgoing.push_str(&format!("\"frequency_in_hz\": {},", results.frequency));
+    outgoing.push_str(&format!("\"period_in_secs\": {},", results.period));
+    outgoing.push_str(&format!("\"capacitance_in_f\": {},", results.cap_f));
+    outgoing.push_str(&format!("\"capacitance_in_uf\": {},", results.cap_uf));
+    outgoing.push_str(&format!("\"capacitance_in_nf\": {}", results.cap_nf));
+    outgoing.push_str("}");
 
     let tmpdir = env::temp_dir();
-    let export_path = Path::new(&tmpdir).join("nos.txt");
+    let export_path = Path::new(&tmpdir).join("nos.json");
 
     match fs::write(export_path.as_os_str(), outgoing) {
         Ok(()) => println!("Exported results to {}", export_path.display()),
