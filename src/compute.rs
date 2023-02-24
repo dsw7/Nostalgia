204422ea07d0b5f8e1fdf4b4fe8bb2c77f27548f
David Weber <dsw7@sfu.ca>
2023-02-23 23:47:36 -0800

    Write results file to platform specific tmpdir

--- src/compute.rs
+++ src/compute.rs
@@ -1,4 +1,6 @@
 use std::fs;
+use std::env;
+use std::path::Path;
 
 const R1: f32 = 99.5 * 1000.;
 const R2: f32 = 21.5 * 1000.;
@@ -34,17 +36,22 @@ struct Results {
 fn export_data_to_file(results: &Results) {
 
     let mut outgoing = String::new();
+
+    outgoing.push_str("# Nostalgia generated results file. This file can be safely deleted\n");
+    outgoing.push_str("# See https://github.com/dsw7/Nostalgia for more information\n");
+
     outgoing.push_str(&format!("Frequency (Hz): {}\n", results.frequency));
     outgoing.push_str(&format!("Period (s): {}\n", results.period));
     outgoing.push_str(&format!("Capacitance (F): {}\n", results.cap_f));
     outgoing.push_str(&format!("Capacitance (uF): {}\n", results.cap_uf));
     outgoing.push_str(&format!("Capacitance (nF): {}\n", results.cap_nf));
 
-    let export_path = "/tmp/nos.txt";
+    let tmpdir = env::temp_dir();
+    let export_path = Path::new(&tmpdir).join("nos.txt");
 
-    match fs::write(export_path, outgoing) {
-        Ok(()) => println!("Exported results to {}", export_path),
-        Err(error) => panic!("Could not export to {}. The error was {}", export_path, error),
+    match fs::write(export_path.as_os_str(), outgoing) {
+        Ok(()) => println!("Exported results to {}", export_path.display()),
+        Err(error) => panic!("Could not export to {}. The error was {}", export_path.display(), error),
     };
 }
 
